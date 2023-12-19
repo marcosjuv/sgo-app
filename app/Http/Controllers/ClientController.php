923aed8bf339c63c53663c3ae864c8f969c7a0a3
marcosjuv <mjuv_1978@hotmail.com>
2023-12-19 17:03:38 -0400

    create and update client

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,11 +5,23 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Client;
 use App\Http\Resources\ClientCollection;
+use App\Http\Resources\ClientResource;
 
 class ClientController extends Controller
 {
     public function index(){
         $clientes = Client::paginate();
         return new ClientCollection($clientes);
+    }
+
+
+    public function Store(Request $request)
+    {
+        return new ClientResource(Client::create($request->all()));
+    }
+
+    public function Update(Request $request, Client $client)
+    {
+        return $client->update($request->all());
     }
 }
